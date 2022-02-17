79fd6de5bc13c0fcdb77b1c6fd93eacad320f7af
Jisung <jisungkim215@gmail.com>
2022-02-16 21:59:41 -0500

    Updated ModifyPage

--- JKFeedbackA2/js/jkfacade.js
+++ JKFeedbackA2/js/jkfacade.js
@@ -5,10 +5,18 @@
  *          Jisung Kim, 2022-02-17 : Created
  */
 
-function showCalculatedRating(){
+function showCalculatedRatingAdd(){
     var quality = parseInt($("#numFoodQualityAdd").val());
     var service = parseInt($("#numServiceAdd").val());
     var value = parseInt($("#numValueAdd").val());
 
     $("#txtOverallRatingAdd").val(getOverallRating(quality, service, value) + "%");
+}
+
+function showCalculatedRatingModify(){
+    var quality = parseInt($("#numFoodQualityModify").val());
+    var service = parseInt($("#numServiceModify").val());
+    var value = parseInt($("#numValueModify").val());
+
+    $("#txtOverallRatingModify").val(getOverallRating(quality, service, value) + "%");
 }
--- JKFeedbackA2/js/jkglobal.js
+++ JKFeedbackA2/js/jkglobal.js
@@ -7,25 +7,48 @@
 
 function chkRatingsAdd_change() {
     if ($("#chkRatingsAdd").prop("checked")){
-        $(".ratingBoxes").show();
+        $("#ratingBoxesAdd").show();
         $("#numFoodQualityAdd").val("0");
         $("#numServiceAdd").val("0");
         $("#numValueAdd").val("0");
     } else {
-        $(".ratingBoxes").hide();
+        $("#ratingBoxesAdd").hide();
     }
 }
 
 function numFoodQualityAdd_change() {
-    showCalculatedRating();
+    showCalculatedRatingAdd();
 }
 
 function numServiceAdd_change() {
-    showCalculatedRating();
+    showCalculatedRatingAdd();
 }
 
 function numValueAdd_change() {
-    showCalculatedRating();
+    showCalculatedRatingAdd();
+}
+
+function chkRatingsModify_change() {
+    if ($("#chkRatingsModify").prop("checked")){
+        $("#ratingBoxesModify").show();
+        $("#numFoodQualityModify").val("0");
+        $("#numServiceModify").val("0");
+        $("#numValueModify").val("0");
+    } else {
+        $("#ratingBoxesModify").hide();
+    }
+}
+
+function numFoodQualityModify_change() {
+    showCalculatedRatingModify();
+}
+
+function numServiceModify_change() {
+    showCalculatedRatingModify();
+}
+
+function numValueModify_change() {
+    showCalculatedRatingModify();
 }
 
 function init() {
@@ -33,6 +56,11 @@ function init() {
     $("#numFoodQualityAdd").on("change", numFoodQualityAdd_change);
     $("#numServiceAdd").on("change", numServiceAdd_change);
     $("#numValueAdd").on("change", numValueAdd_change);
+
+    $("#chkRatingsModify").on("change", chkRatingsModify_change);
+    $("#numFoodQualityModify").on("change", numFoodQualityModify_change);
+    $("#numServiceModify").on("change", numServiceModify_change);
+    $("#numValueModify").on("change", numValueModify_change);
 }
 
 $(document).ready(function () {
